5be93906bbc9829398810e36639048ea92408a47
Anib <saliv.mah@gmail.com>
2022-08-28 21:34:28 +0545

    logical

---
+++ base.js
@@ -0,0 +1,11 @@
+function createBase(baseNumber) {
+  return function (N) {
+    // we are referencing baseNumber here even though it was declared
+    // outside of this function. Closures allow us to do this in JavaScript
+    return baseNumber + N;
+  };
+}
+
+var addSix = createBase(6);
+addSix(10);
+addSix(21);
